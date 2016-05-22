0233fa848a556a7ff97015a4d39deb92b7b301f9
root <root@localhost.localdomain>
2016-05-22 20:29:03 +0300

    initial commit

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+#!/usr/bin/env bash
+
+ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
+
+gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+gunicorn -c /home/box/web/etc/django.py wsgi --daemon

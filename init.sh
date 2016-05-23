f9f980192c198e959a568743f207f281169a34dd
valera-slk <valera.slk@gmail.com>
2016-05-23 09:30:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon

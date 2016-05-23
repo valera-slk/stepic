c1321be2bfbb816055ae957ba052bcf93525a931
valera-slk <valera.slk@gmail.com>
2016-05-23 09:24:57 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('qa.views',
     url(r'^$', 'qa.views.test'),
     url(r'^login/$', 'qa.views.test'),
     url(r'^signup/$', 'qa.views.test'),

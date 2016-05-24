b3636a601b7f51f9412db55de565c3b6d97669f7
valera-slk <valera.slk@gmail.com>
2016-05-24 09:09:21 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,11 +1,11 @@
 from qa.views import test
 
-urlpatterns = patterns('qa.views',
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/$', 'qa.views.test'),
-    url(r'^signup/$', 'qa.views.test'),
-    url(r'^question/(\d+)$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/$', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),
-)
+urlpatterns = [
+    url(r'^$', 'qa.views.test', name = 'test'),
+    url(r'^login/$', 'qa.views.test', name = 'test'),
+    url(r'^signup/$', 'qa.views.test', name = 'test'),
+    url(r'^question/(\d+)$', 'qa.views.test', name = 'test'),
+    url(r'^ask/.*$', 'qa.views.test', name = 'test'),
+    url(r'^popular/$', 'qa.views.test', name = 'test'),
+    url(r'^new/$', 'qa.views.test', name = 'test'),
+]
